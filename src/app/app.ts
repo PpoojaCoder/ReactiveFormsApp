05031eda01a5fad538dbe31219a0f5ca5e4aa4dc
PpoojaCoder <151538200+PpoojaCoder@users.noreply.github.com>
2026-04-13 16:21:54 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+// this is model class defining structure of form
 export class App {
-  protected readonly title = signal('ReactiveFormsApp');
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
